02bcb8750f830453c22b39fedd1fb719f9d14e77
Adam Schaal <amschaal@gmail.com>
2022-12-06 16:16:33 -0800

    working on adding order creation

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
+from rest_framework import exceptions
 from dnaorder.models import Submission
 from plugins import plugin_submission_decorator
 import csv
@@ -22,10 +23,31 @@ def post_data(submission, params):
 # @csrf_exempt
 
 @api_view(['GET'])
-@permission_classes((AllowAny,))
 @plugin_submission_decorator(permissions=['VIEW'], all=True)
 def get_services(request, submission):
     response = post_data(submission, {"action":"getservices"})
     lines = [l.decode('utf-8') for l in response.readlines()]
-    services = csv.reader(lines)
+    services = csv.DictReader(lines)
     return Response({'submission':submission.id, 'services': services})
+
+@api_view(['POST'])
+@plugin_submission_decorator(permissions=['STAFF'], all=True)
+def create_order(request, submission):
+    plugin_data = submission.plugin_data.get('ppms',{})
+
+    if 'orders' in plugin_data:
+        pass
+        # raise exceptions.NotAcceptable('Submission already has an order')
+    else:
+        plugin_data['orders'] = []
+    try:
+        response = post_data(submission, {"action":"createorder","login": request.POST.get("username"), "serviceid": request.POST.get("serviceid"), "quantity": request.POST.get("quantity"), "Scomments": "Created for submission "+submission.get_absolute_url(True)})
+    except Exception as e:
+        raise exceptions.NotAcceptable(str(e))
+    order = response.read().decode('utf-8')
+    plugin_data['orders'].append(order)
+    submission.plugin_data['ppms'] = plugin_data
+    submission.save()
+    # lines = [l.decode('utf-8') for l in response.readlines()]
+    # order = csv.DictReader(lines)
+    return Response({'submission':submission.id, 'plugin_data': plugin_data})
--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import api
 
 urlpatterns = [
-    path('services/', api.get_services, name='ppms_services')
+    path('services/', api.get_services, name='ppms_services'),
+    path('create_order/', api.create_order, name='create_order')
 ]

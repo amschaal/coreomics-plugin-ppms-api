36aa131e0876bceed4cabfea5fcedcab302b1005
Adam Schaal <amschaal@gmail.com>
2025-10-31 13:27:47 -0700

    More work on submission PI mapping using PPMS api

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
 from dnaorder.models import PI, PIInstitution
 from .api import get_group, get_user_info
 
+def map_submission_pi(submission, save=True):
+    from .plugin import PPMSPlugin
+    settings = submission.lab.get_plugin_settings_by_id(PPMSPlugin.ID, private=True, institution=True)
+    pi = get_or_create_pi(settings, submission.pi_email)
+    submission.pi = pi
+    if save:
+        submission.save()
+    return pi
+
 def get_or_create_pi(settings, email):
     email = email.strip().lower()
-    pi = PI.objects.filter(email = email).first()
+    pi = PI.objects.filter(email__iexact = email).first()
     if pi:
         return pi
     user_info = get_user_info(settings, email)
--- validator.py
+++ validator.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from dnaorder.models import PI
+from .utils import get_or_create_pi
 
 def validate_submission(plugin, data, serializer):
     email = data.get('pi_email')
     pi = PI.get_pi(email)
     if not pi:
-        raise serializers.ValidationError({'pi_email':f'No group has been registered in the system with the email "{email}".'})
+        lab = getattr(serializer, '_lab', None)
+        if lab:
+            settings = lab.get_plugin_settings_by_id(plugin.ID, private=True, institution=True)
+            pi = get_or_create_pi(settings, email)
+            if not pi:
+                raise serializers.ValidationError({'pi_email':f'No group has been registered in the system with the email "{email}", lab ({serializer._lab.name}).'})
+    serializer.instance.pi = pi
     return data

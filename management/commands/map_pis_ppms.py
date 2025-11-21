5a990c680dc4499f0d15961137e3a6a2542557f4
Adam Schaal <amschaal@gmail.com>
2025-11-21 15:34:58 -0800

    refactor map_pis_ppms management command

--- management/commands/map_pis_ppms.py
+++ management/commands/map_pis_ppms.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime, timedelta
-from dnaorder.models import PIInstitution, Submission, Lab, PI
+from dnaorder.models import PIInstitution, Submission, Institution, PI
 from plugins.ppms import api
+from plugins.ppms.models import PPMSGroup
 from plugins.ppms.utils import import_ppms_group
 
 class Command(BaseCommand):
@@ -20,18 +21,6 @@ class Command(BaseCommand):
         days = options['days']
         since_date = datetime.now() - timedelta(days=days)
         self.bulk_map(since=since_date)
-        # for s in Submission.objects.filter(updated__gte=since_date, pi_email__contains='@', pi__isnull=True):
-        #     s.map_pi()
-
-    def get_pi_map(self, settings):
-        if self.pi_map:
-            return self.pi_map
-        groups = api.get_groups(settings)
-        pis = {}
-        for g in groups:
-            pis[g['head'].strip().lower()] = g
-        self.pi_map = pis
-        return self.pi_map
 
     def bulk_map(self, since):
         # Create map of existing PIs
@@ -43,17 +32,10 @@ class Command(BaseCommand):
         submissions = []
         unmapped = []
 
-        settings = None
-        for lab in Lab.objects.all():
-            settings = api.get_lab_settings(lab)
-            if settings:
-                break
-        
-        pi_map = self.get_pi_map(settings)
+        for i in Institution.objects.all():
+            pi_map = PPMSGroup.get_group_map(i)
 
-        for lab in Lab.objects.all():
-            print(lab, settings)
-            for s in Submission.objects.filter(pi_email__contains='@', lab=lab, pi__isnull=True):
+            for s in Submission.objects.filter(pi_email__contains='@', lab__institution=i, pi__isnull=True):
                 email = s.pi_email.strip().lower()
                 pi = existing_pis.get(email)
                 if pi:
@@ -63,10 +45,6 @@ class Command(BaseCommand):
                     ppms_pi = pi_map.get(email)
                     if ppms_pi:
                         ppms_group = import_ppms_group(email, ppms_pi)
-                        # institution = PIInstitution.objects.filter(name__iexact=ppms_pi['institution']).first()
-                        # if not institution:
-                        #     institution = PIInstitution.objects.create(name=ppms_pi['institution'][:75])
-                        # pi = PI.objects.create(email=email, first_name=s.pi_first_name, last_name=s.pi_last_name, phone=s.pi_phone, department=ppms_pi['department'][:75], institution=institution, meta={'ppms':ppms_pi})
                         existing_pis[email] = ppms_group.pi
                         s.pi = ppms_group.pi
                         s.save()
